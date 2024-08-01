47291af9d755f1de7de70d97603bdcaa28055d74
vukedd <vukedd@gmail.com>
2024-08-01 15:22:58 +0200

    Initial commit

---
+++ src/test/java/com/webshop/main/WebShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webshop.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
